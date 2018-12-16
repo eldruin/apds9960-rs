46195287c34681ae4d2614cfb0122d9146463c12
Diego Barrios Romero <eldruin@gmail.com>
2018-12-16 14:56:32 +0100

    Shorten test names

--- tests/light.rs
+++ tests/light.rs
@@ -4,6 +4,6 @@ use hal::i2c::Transaction as I2cTrans;
 mod common;
 use common::{new, destroy, BitFlags, Register, DEV_ADDR};
 
-write_test!(can_enable_light, enable_light, ENABLE, BitFlags::AEN);
-write_test!(can_disable_light, disable_light, ENABLE, 0);
+write_test!(enable, enable_light, ENABLE, BitFlags::AEN);
+write_test!(disable, disable_light, ENABLE, 0);
 
