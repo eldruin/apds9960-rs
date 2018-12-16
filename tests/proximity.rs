ce017e765ae90884bce34b7c37f85f16fdde9168
Diego Barrios Romero <eldruin@gmail.com>
2018-12-16 14:47:58 +0100

    Shorten test names

--- tests/proximity.rs
+++ tests/proximity.rs
@@ -4,18 +4,18 @@ use hal::i2c::Transaction as I2cTrans;
 mod common;
 use common::{new, destroy, BitFlags, Register, DEFAULT_CONFIG2, DEV_ADDR};
 
-write_test!(can_enable_proximity, enable_proximity, ENABLE, BitFlags::PEN);
-write_test!(can_disable_proximity, disable_proximity, ENABLE, 0);
-write_test!(en_prox_int, enable_proximity_interrupts, ENABLE, BitFlags::PIEN);
-write_test!(dis_prox_int, disable_proximity_interrupts, ENABLE, 0);
-write_test!(en_prox_sat_int, enable_proximity_saturation_interrupts, CONFIG2, BitFlags::PSIEN | DEFAULT_CONFIG2);
-write_test!(dis_prox_sat_int, disable_proximity_saturation_interrupts, CONFIG2, DEFAULT_CONFIG2);
+write_test!(can_enable, enable_proximity, ENABLE, BitFlags::PEN);
+write_test!(can_disable, disable_proximity, ENABLE, 0);
+write_test!(en_int, enable_proximity_interrupts, ENABLE, BitFlags::PIEN);
+write_test!(dis_int, disable_proximity_interrupts, ENABLE, 0);
+write_test!(en_sat_int, enable_proximity_saturation_interrupts, CONFIG2, BitFlags::PSIEN | DEFAULT_CONFIG2);
+write_test!(dis_sat_int, disable_proximity_saturation_interrupts, CONFIG2, DEFAULT_CONFIG2);
 
-write_test!(set_prox_low_th, set_proximity_low_threshold, PILT, 0xAB, 0xAB);
-write_test!(set_prox_high_th, set_proximity_high_threshold, PIHT, 0xAB, 0xAB);
+write_test!(set_low_th, set_proximity_low_threshold, PILT, 0xAB, 0xAB);
+write_test!(set_high_th, set_proximity_high_threshold, PIHT, 0xAB, 0xAB);
 
-write_test!(set_prox_ur_off, set_proximity_up_right_offset, POFFSET_UR, 55, 55);
-write_test!(set_prox_fl_off, set_proximity_down_left_offset, POFFSET_DL, 55, 55);
+write_test!(set_ur_off, set_proximity_up_right_offset, POFFSET_UR, 55, 55);
+write_test!(set_fl_off, set_proximity_down_left_offset, POFFSET_DL, 55, 55);
 
 #[test]
 fn can_set_poffsets() {
@@ -30,8 +30,8 @@ fn can_set_poffsets() {
     destroy(sensor);
 }
 
-read_test!(can_read_pvalid_true,  is_proximity_data_valid, true, STATUS, BitFlags::PVALID);
-read_test!(can_read_pvalid_false, is_proximity_data_valid, false, STATUS, 0);
+read_test!(is_pvalid_true,  is_proximity_data_valid, true, STATUS, BitFlags::PVALID);
+read_test!(is_pvalid_false, is_proximity_data_valid, false, STATUS, 0);
 
 read_test!(can_read_prox, read_proximity, 0x12, STATUS, BitFlags::PVALID, PDATA, 0x12);
 
